71c0b605c566dd21a60db66f96208aa91fea3f34
ad-kd <12218adhithyaksnm@gmail.com>
2025-12-29 18:34:13 +0530

    add-6

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+//middleware to handle a cors
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+app.listen(prototype, () => console.log(`Server runnning on port ${PORT}`));
