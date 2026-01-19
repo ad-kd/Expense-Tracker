4096c15decd7d9e461f4dd0bbea90f9ba98bb892
Adhithya K <12218adhithyaksnm@gmail.com>
2026-01-19 18:21:27 +0530

    Added Income Routes

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,5 +1,4 @@
-
-const User = require("../models/User");
+const xlsx = require("xlsx");
 const Income = require("../models/Income");
 
 //Add Income Source
@@ -30,10 +29,47 @@ exports.addIncome = async (req, res) => {
 }
 
 //Get All Income Source
-exports.getAllIncome = async (req, res) => {};
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1});
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
 
 //Delete Income Source
-exports.deleteIncome = async (req, res) => {};
+exports.deleteIncome = async (req, res) => {
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({ messsage: "Income Deleted Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
 
 //To Download a Income Excel Doc
-exports.downloadIncomeExcel = async (req, res) => {};
+exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1});
+
+        //Prepare data for excel
+
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+        xlsx.writeFile(wb, "income_details.xlsx");
+        res.download("income_details.xlsx");
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -6,8 +6,8 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/add", protect, addIncome);
-// router.get("/get", protect, getAllIncome);
-// router.get("/downloadexcel", protect, downloadIncomeExcel);
+router.get("/get", protect, getAllIncome);
+router.get("/downloadexcel", protect, downloadIncomeExcel);
 router.delete("/:id", protect, deleteIncome);
 
 module.exports = router;
