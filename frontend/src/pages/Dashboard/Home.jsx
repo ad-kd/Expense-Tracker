3a42a0415113dc503d4639618c4fbba9cbf245a3
Adhithya K <12218adhithyaksnm@gmail.com>
2026-01-26 19:26:55 +0530

    New Error at expense card at home.jsx

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -10,6 +10,9 @@ import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import {IoMdCard} from 'react-icons/io'
 import { addThousandsSeparator } from '../../utils/helper';
 import RecentTransactions from '../../components/Dashboard/RecentTransactions';
+import FinanceOverview from '../../components/Dashboard/FinanceOverview';
+import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
+import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
 
 const Home = () => {
   useUserAuth();
@@ -46,7 +49,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu='Dashboard'>
       <div className='my-5 mx-auto'>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
             icon={<IoMdCard />}
             label="Total Balance"
@@ -67,13 +70,28 @@ const Home = () => {
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color="bg-red-500"
           />
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-          <RecentTransactions
-            transactions={dashboardData?.RecentTransactions}
+          {/* <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate('/expense')}
           />
+
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+            /> */}
+
+            <ExpenseTransactions 
+              transactions={dashboardData?.last30DaysExpense?.transactions || []}
+              onSeeMore={() => navigate("/expense")}
+            />
+
+            <Last30DaysExpenses
+              data={dashboardData?.last30DaysExpense?.transactions || 0}
+            />
         </div>
 
       </div>
